5d967f993bf2327437fde4a8fbddec8154f85976
morgaeff2017 <59146614+morgaeff2017@users.noreply.github.com>
2019-12-26 13:07:35 +0300

    Update Lab1_DB.java

--- src/java/lab1_db/Lab1_DB.java
+++ src/java/lab1_db/Lab1_DB.java
@@ -12,7 +12,6 @@ import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
-import org.hibernate.*;
 
 /**
  *
@@ -23,16 +22,23 @@ public class Lab1_DB {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {     
+    public static void main(String[] args) {
+        // создаём объект sf типа SessionFacory в который записываем то, что вернул нам 
+        //метод getSessionFactory класса NewHibernateUtil
         SessionFactory sf = NewHibernateUtil.getSessionFactory();
+        // создаём объект s типа Session и записываем туда то, что вернул метод openSession() объекта sf
         Session s = sf.openSession();
+        //создаём запрос
         Query q = s.createQuery("from Gruppyi g");
+        //создаём список с с типом данных Gruppyi в который записываем то,что вернул нам метод list
         List<Gruppyi> gruppyi = q.list();
+        //заполняем список через цкил
         for (Gruppyi group : gruppyi)
         {
             List<Studentyi> students = group.getStudentyis();
             for(Studentyi student : students){
                 System.out.println(student.getNomerZachetki());
+    
                 System.out.println(student.getGruppyi());
                 System.out.println(student.getFamiliya());
                System.out.println( student.getImya());
